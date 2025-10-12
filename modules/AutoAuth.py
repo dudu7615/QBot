d0838b0d02b019d140d3b19abc830b76935cca42
dudu7615 <dudu7615@qq.com>
2025-10-12 17:01:01 +0800

    fix: 循环导入

--- modules/AutoAuth.py
+++ modules/AutoAuth.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 from datetime import datetime, timedelta
-from modules.HttpSender import client
+from httpx import AsyncClient
 from modules.Logger import logger
 from modules import Urls
 
@@ -11,7 +11,7 @@ class AccessToken:
     expires_in: datetime
 
 
-async def getAccessToken(appId: str, appSecret: str) -> AccessToken | None:
+async def getAccessToken(appId: str, appSecret: str, client: AsyncClient) -> AccessToken | None:
     url = Urls.getAppAccessToken
     headers = {"Content-Type": "application/json"}
     data = {"appId": appId, "clientSecret": appSecret}
@@ -19,9 +19,13 @@ async def getAccessToken(appId: str, appSecret: str) -> AccessToken | None:
     if response.status_code == 200:
         logger.info(f"获取AccessToken成功: {response.json()}")
         resp_json = response.json()
+        logger.debug(f"获取AccessToken成功: {resp_json}")
         return AccessToken(
-            access_token=resp_json.get("accessToken"),
-            expires_in=datetime.now() + timedelta(seconds=resp_json.get("expiresIn")-30),  # -30 to keep it usable and reget on time
+            access_token=resp_json.get("access_token"),
+            expires_in=datetime.now()
+            + timedelta(
+                seconds=int(resp_json.get("expires_in")) - 30
+            ),  # -30 to keep it usable and reget on time
         )
     else:
         logger.error(f"获取AccessToken失败: {response.status_code} - {response.text}")
