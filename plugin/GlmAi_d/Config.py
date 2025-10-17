7183a8e3539b31465f0e10cd554e74a483916da2
dudu7615 <dudu7615@qq.com>
2025-10-17 20:33:11 +0800

    fix: read file that path from file defined

--- plugin/GlmAi_d/Config.py
+++ plugin/GlmAi_d/Config.py
@@ -1,13 +1,10 @@
 from typing import TypedDict
 import yaml
-from pathlib import Path
-
-PLUGIN_ROOT = Path(__file__).parent
-
+from plugin.GlmAi_d import Paths
 class Config(TypedDict):
     apiKey: str
 
 config: Config = yaml.load(
-    open(PLUGIN_ROOT / "config.yaml", "r", encoding="utf8"),
+    open(Paths.CONFIG_FILE, "r", encoding="utf8"),
     Loader=yaml.FullLoader,
 )
