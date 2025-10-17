4e2298d4638adf668edd855d7d90f6091b885499
dudu7615 <dudu7615@qq.com>
2025-10-17 23:59:18 +0800

    fix: auto create dir and files

--- plugin/GlmAi_d/History.py
+++ plugin/GlmAi_d/History.py
@@ -4,10 +4,19 @@ import yaml
 class PersonHistory:
     def __init__(self, openId: str):
         self.openId = openId
-        self.history: list[Types.ApiJson_Messages] = yaml.load(
-            open(Paths.HISTORY / f"{self.openId}.yaml", "r+", encoding="utf-8"),
-            Loader=yaml.FullLoader,
-        )
+        self.path = Paths.HISTORY / f"{self.openId}.yaml"
+        if not self.path.exists():
+            self.history = []
+            yaml.dump(
+                self.history,
+                open(Paths.HISTORY / f"{self.openId}.yaml", "w+", encoding="utf-8"),
+                Dumper=yaml.Dumper,
+            )
+        else:
+            self.history: list[Types.ApiJson_Messages] = yaml.load(
+                open(Paths.HISTORY / f"{self.openId}.yaml", "r+", encoding="utf-8"),
+                Loader=yaml.FullLoader,
+            )
     
     def addHistory(self, message: Types.ApiJson_Messages) -> None:
         self.history.append(message)
--- plugin/GlmAi_d/Paths.py
+++ plugin/GlmAi_d/Paths.py
@@ -4,3 +4,4 @@ ROOT = Path(__file__).parent
 CONFIG_FILE = ROOT / "config.yaml"
 DATA = ROOT / "data"
 HISTORY = DATA / "history"
+HISTORY.mkdir(parents=True, exist_ok=True)
