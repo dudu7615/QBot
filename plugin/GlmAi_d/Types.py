ca56dfd600ed45e35f67fffd96bc5319d32abe22
dudu7615 <dudu7615@qq.com>
2025-10-17 20:32:01 +0800

    fix: 将同类合并为别名

--- plugin/GlmAi_d/Types.py
+++ plugin/GlmAi_d/Types.py
@@ -1,28 +1,30 @@
 from typing import TypedDict
 
+
 class ApiJson_Messages(TypedDict):
-    role: str # system, user, assistant, tool
+    role: str  # system, user, assistant, tool
     content: str
+
 
 class ApiJson(TypedDict):
     model: str  # glm-4.5-flash
     messages: list[ApiJson_Messages]
-    temperature: float # 1
-    max_tokens: int # 65536
-    stream: bool # False
+    temperature: float  # 1
+    max_tokens: int  # 65536
+    stream: bool  # False
     thinking: dict[str, str]  # {"type": "enabled"|"disabled"}
-    do_sample: bool # True
+    do_sample: bool  # True
     top_p: float  # 0.95
-    
 
-class ReplyJson_Choices_Message(TypedDict):
-    role: str
-    content: str
+
+class ReplyJson_Choices_Message(ApiJson_Messages): ...
+
 
 class ReplyJson_Choices(TypedDict):
     index: int
     message: ReplyJson_Choices_Message
     finish_reason: str
+
 
 class ReplyJson(TypedDict):
     id: str
@@ -31,3 +33,7 @@ class ReplyJson(TypedDict):
     model: str
     choices: list[ReplyJson_Choices]
     usage: dict[str, int]
+
+    time: str
+    role: str
+    content: str
