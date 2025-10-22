6821efa451afe0a0c7739dc44a353c66bbcec941
dudu <dudu7615@qq.com>
2025-10-22 19:10:16 +0800

    fix: save yml files with unicode

--- plugin/GlmAi_d/History.py
+++ plugin/GlmAi_d/History.py
@@ -6,33 +6,24 @@ class PersonHistory:
         self.openId = openId
         self.path = Paths.HISTORY / f"{self.openId}.yaml"
         if not self.path.exists():
-            self.history = []
-            yaml.dump(
-                self.history,
-                open(Paths.HISTORY / f"{self.openId}.yaml", "w+", encoding="utf-8"),
-                Dumper=yaml.Dumper,
-            )
+            with open(Paths.HISTORY / f"{self.openId}.yaml", "w+", encoding="utf-8") as f:
+                yaml.dump(self.history, f, allow_unicode=True)
         else:
-            self.history: list[Types.ApiJson_Messages] = yaml.load(
-                open(Paths.HISTORY / f"{self.openId}.yaml", "r+", encoding="utf-8"),
-                Loader=yaml.FullLoader,
-            )
+            with open(Paths.HISTORY / f"{self.openId}.yaml", "r+", encoding="utf-8") as f:
+                self.history: list[Types.ApiJson_Messages] = yaml.load(f, Loader=yaml.FullLoader)
+
+        if not self.history:
+            self.history = []
     
     def addHistory(self, message: Types.ApiJson_Messages) -> None:
         self.history.append(message)
-        yaml.dump(
-            self.history,
-            open(Paths.HISTORY / f"{self.openId}.yaml", "w+", encoding="utf-8"),
-            Dumper=yaml.Dumper,
-        )
+        with open(Paths.HISTORY / f"{self.openId}.yaml", "w+", encoding="utf-8") as f:
+            yaml.dump(self.history, f, allow_unicode=True)
 
     def getHistory(self) -> list[Types.ApiJson_Messages]:
         return self.history
     
     def clearHistory(self) -> None:
         self.history = []
-        yaml.dump(
-            self.history,
-            open(Paths.HISTORY / f"{self.openId}.yaml", "w+", encoding="utf-8"),
-            Dumper=yaml.Dumper,
-        )
+        with open(Paths.HISTORY / f"{self.openId}.yaml", "w+", encoding="utf-8") as f:
+            yaml.dump(self.history, f, allow_unicode=True, Dumper=yaml.Dumper)
