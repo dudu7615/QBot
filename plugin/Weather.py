56179715ec91c150ee5f933b62e2e3d62450a282
dudu7615 <dudu7615@qq.com>
2026-01-11 15:55:37 +0800

    feat: 插件本体

--- plugin/Weather.py
+++ plugin/Weather.py
@@ -2,12 +2,12 @@ from modules.Logger import logger
 from modules.Types import MessageData, MessageType
 from modules import HttpSender
 from plugin.PluginBase import PluginBase
-from plugin.GlmAi_d import RequApi
-from plugin.GlmAi_d.History import PersonHistory
+from plugin.Weather_d import SqlCache, GetWeather
 
 
 class Weather(PluginBase):
     usable = True
+    messageStartWith = ["/今日天气", "/天气预报", "/常住地"]
 
     @property
     def messageType(self) -> MessageType.Group | MessageType.Person:
@@ -20,17 +20,16 @@ class Weather(PluginBase):
     async def run(self, message: MessageData):
         content = message["d"]["content"]
         author = message["d"]["author"]
-        if content.startswith("!") or content.startswith("！"):
-            command = content.split(" ")[0][1:]
-            match command:
-                case "clear":
-                    PersonHistory(author["id"]).clearHistory()
-                    
-                    await HttpSender.sendText2Person("已清空历史记录", message, author["id"])
-                case _:
-                    logger.warning(f"未知命令: {command}")
-                    await HttpSender.sendText2Person("使用 “!clear” 清空历史记录", message, author["id"])
-                    
-        else:
-            reply = await RequApi.RequApi(content, author["id"])
-            await HttpSender.sendText2Person(reply, message, author["id"])
+        if content.startswith("/今日天气"):
+            if len(content.split(" ")) == 2:
+                city = content.split(" ")[1]
+                weather = await GetWeather.getTodayWeather(city)
+            elif len(content.split(" ")) == 1:
+                city = SqlCache.User.get(author["id"])
+                if city:
+                    weather = await GetWeather.getTodayWeather(city)
+                else:
+                    weather = "请先设置常住地"
+            else:
+                weather = "输入格式错误. 请输入 /今日天气 <城市名(若已设置常住地, 可省略)>"
+            await HttpSender.sendText2Person(weather, message, author["id"])
