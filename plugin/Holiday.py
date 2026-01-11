49efd73946dffb897dfb399cd74a401493bdff1f
dudu <dudu7615@qq.com>
2026-01-12 00:23:56 +0800

    V1.3.0 假期查询功能 (#7)
    * env: updata
    
    * env: gitignore
    
    * feat: sqlite缓存
    
    * feat: 测试api
    
    * feat: 请求api获取信息
    
    * feat: 完成插件
    
    * fix: 设置请求超时
    
    * fix: 异步检查api, 避免阻塞
    
    * fix: 删除无用变量
    
    * fix: 防止日期带有时间

---
+++ plugin/Holiday.py
@@ -0,0 +1,42 @@
+from modules.Logger import logger
+from modules.Types import MessageData, MessageType
+from modules import HttpSender
+from plugin.PluginBase import PluginBase
+from plugin.Holiday_d.SqlCache import Cache
+from plugin.Holiday_d import RequApi
+from datetime import datetime
+
+
+class Holiday(PluginBase):
+    usable = True
+    messageStartWith = "/最近假期"
+
+    _checked = False
+
+
+    @property
+    def messageType(self) -> MessageType.Group | MessageType.Person:
+        return MessageType.Person.C2C_MESSAGE_CREATE
+
+    @property
+    def pluginName(self) -> str:
+        return "Holiday"
+
+    async def run(self, message: MessageData):
+        if not self._checked:
+            RequApi.checkAndUpdata()
+            self._checked = True
+        
+        author = message["d"]["author"]
+        
+        nextHoliday =  Cache.getNextHoliday(datetime.now())
+        nextDeHoliday = Cache.getNextDeHoliday(datetime.now())
+
+        if nextHoliday is None:
+            reply = "没有找到下一个假期"
+        elif nextDeHoliday is None:
+            reply = f"下一个假期是{nextHoliday.name}，日期为{nextHoliday.date.strftime('%Y-%m-%d')}"
+        else:
+            reply = (f"下一个假期是{nextHoliday.name}，日期为{nextHoliday.date.strftime('%Y-%m-%d')}\n"
+                     f"下一个调休是{nextDeHoliday.name}，日期为{nextDeHoliday.date.strftime('%Y-%m-%d')}")
+        await HttpSender.sendText2Person(reply, message, author["id"])
